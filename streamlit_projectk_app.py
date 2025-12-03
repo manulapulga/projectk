754efcd3b6ade054ab414d4797dc63f07fb6cfca
manulapulga <manupoolakkalgtalk@gmail.com>
2025-12-04 00:11:27 +0530

    Initial commit 04-12-2025  0:11:27.87

--- streamlit_projectk_app.py
+++ streamlit_projectk_app.py
@@ -75,8 +75,8 @@ def inject_custom_css():
     /* Main content container – prevents header overlap */
     .block-container {{
         padding-top: 0.1rem !important;   /* header clearance */
-        padding-left: 0.1rem !important;
-        padding-right: 0.1rem !important;
+        padding-left: 0.5rem !important;
+        padding-right: 0.5rem !important;
         padding-bottom: 1rem !important;
     }}
 
