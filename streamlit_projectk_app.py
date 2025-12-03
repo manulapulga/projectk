2ac7f36c2b1dc67fe95b214600ce66b7eae414d7
manulapulga <manupoolakkalgtalk@gmail.com>
2025-12-03 23:46:30 +0530

    Initial commit 03-12-2025 23:46:29.98

--- streamlit_projectk_app.py
+++ streamlit_projectk_app.py
@@ -1679,7 +1679,7 @@ def show_enhanced_question_interface():
         "Choose an option:",
         options=["A", "B", "C", "D", None],
         format_func=lambda x: "Clear Response" if x is None else f"{x}) {options_dict[x]}",
-        index=[None, "A", "B", "C", "D"].index(default_radio_value),
+        index=["A", "B", "C", "D", None].index(default_radio_value),
         key=f"radio_{current_idx}"
     )
     
