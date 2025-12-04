62d623a7e53a43da84c22f4abc67fb0f91abee54
manulapulga <manupoolakkalgtalk@gmail.com>
2025-12-04 22:21:11 +0530

    Initial commit 04-12-2025 22:21:11.34

--- streamlit_projectk_app.py
+++ streamlit_projectk_app.py
@@ -1016,7 +1016,7 @@ def show_user_management():
                 
                 with action_col3:
                     # Edit user (placeholder)
-                    if st.button("✏ Edit", key=f"edit_{row['Username']}", use_container_width=True):
+                    if st.button(✏️ Edit", key=f"edit_{row['Username']}", use_container_width=True):
                         st.info("Edit functionality coming soon!")
                 
                 with action_col4:
